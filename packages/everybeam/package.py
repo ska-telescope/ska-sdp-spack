c228da154aaf15dc5edfc2df9af5f0add4e259f5
Bram Veenboer <bram.veenboer@gmail.com>
2021-09-22 13:14:37 +0200

    Update everybeam package
    * Add missing hdf5 dependency
    * Add slightly older cmake as dependency

--- packages/everybeam/package.py
+++ packages/everybeam/package.py
@@ -16,5 +16,7 @@ class Everybeam(CMakePackage):
     version('0.1.3', commit='0e1339782fa09d91f0cf37c3cf5f4a1aab62cd97', submodules=True)
     version('0.2.0', commit='74fe444e0052d1179126ba4742eec8392336019d', submodules=True)
 
+    depends_on('hdf5+cxx')
     depends_on('casacore@3.3.0', when='@0.1.3')
     depends_on('casacore@3.4.0', when='@0.2.0')
+    depends_on('cmake@3.18.4')
