07a4012b8dcf2a4705cfc19ccfa23444cadb4724
Bram Veenboer <bram.veenboer@gmail.com>
2021-09-24 12:07:56 +0200

    Add dependency versions for everybeam package

--- packages/everybeam/package.py
+++ packages/everybeam/package.py
@@ -16,8 +16,8 @@ class Everybeam(CMakePackage):
     version('0.1.3', commit='0e1339782fa09d91f0cf37c3cf5f4a1aab62cd97', submodules=True)
     version('0.2.0', commit='74fe444e0052d1179126ba4742eec8392336019d', submodules=True)
 
-    depends_on('hdf5+cxx')
+    depends_on('hdf5+cxx@1.10.7')
     depends_on('casacore@3.3.0')
-    depends_on('boost+filesystem+system')
-    depends_on('fftw')
+    depends_on('boost+filesystem+system@1.77.0')
+    depends_on('fftw@3.3.9')
     depends_on('cmake@3.18.4')
