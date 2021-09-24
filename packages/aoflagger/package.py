78315df53d430ad631c7a632f9ac583d68db4a15
Bram Veenboer <bram.veenboer@gmail.com>
2021-09-24 13:42:45 +0200

    Update aoflagger package
    * Remove gtkmm dependency (this pulls in too many other dependencies,
            make sure that it is installed as system package, e.g.
            gtkmm30-devel)
    * Remove version 2.15.0 (it requires an old version of boost, which
            cannot be build with gcc 9.3.0)
    * Add specific versions to dependencies

--- packages/aoflagger/package.py
+++ packages/aoflagger/package.py
@@ -12,15 +12,12 @@ class Aoflagger(CMakePackage):
     homepage = "https://sourceforge.net/projects/aoflagger/"
     git      = "https://gitlab.com/aroffringa/aoflagger.git"
     version('3.1.0', commit='18b70b9836552d7a632c457ffd8822e57a3ebe7b', submodules=True)
-    version('2.15.0', commit='7d461db725e2c044c4b5653db3aa19a30cc7466a', submodules=True)
 
-    depends_on('casacore+python@3.3.0:', when='@2.15.0')
-    depends_on('casacore+python@3.3.0:', when='@3.1.0')
-    depends_on('fftw~mpi@3.3.9:')
-    depends_on('boost+python+numpy@:1.65', when='@2.15.0')
-    depends_on('boost+python+numpy@:1.76', when='@3.1.0')
+    depends_on('casacore+python@3.3.0')
+    depends_on('cfitsio@3.49')
+    depends_on('fftw@3.3.9:')
+    depends_on('boost+python+numpy@1.76.0')
+    depends_on('hdf5@1.10.7')
     depends_on('libxml2')
     depends_on('lapack')
-    depends_on('cfitsio')
     depends_on('lua')
-    depends_on('gtkmm')
