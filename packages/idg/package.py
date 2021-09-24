10fb6fce9b2bcbc7932d78e6ba20d1cc85cb99a6
Bram Veenboer <bram.veenboer@gmail.com>
2021-09-24 11:55:53 +0200

    Add variants to idg package

--- packages/idg/package.py
+++ packages/idg/package.py
@@ -16,21 +16,28 @@ class Idg(CMakePackage):
 
     version('master', commit='f3dce7b1b395d9d37a484fb7cf05079147f34b2d')
 
+    variant('cuda', default=False, description='Enable CUDA support')
+    variant('python', default=False, description='Enable Python support')
+    variant('report', default=False, description='Enable performance reporting')
+
     depends_on('fftw')
     depends_on('openblas')
-    depends_on('cuda')
-    depends_on('python')
+    depends_on('cuda', when='+cuda')
+    depends_on('python', when='+python')
 
     def cmake_args(self):
+        spec = self.spec
         args = [
             self.define('BUILD_LIB_CPU', True),
-            self.define('BUILD_LIB_CUDA', True),
-            self.define('BUILD_WITH_PYTHON', True),
-            self.define('PERFORMANCE_REPORT', True),
+            self.define('BUILD_LIB_CUDA', '+cuda' in spec),
+            self.define('BUILD_WITH_PYTHON', '+python' in spec),
+            self.define('PERFORMANCE_REPORT', '+report' in spec),
         ]
 
         return args
 
     def setup_run_environment(self, env):
         env.prepend_path('PATH', join_path(self.prefix.bin, 'examples', 'cxx'))
-        env.prepend_path('PATH', join_path(self.prefix.bin, 'examples', 'python'))
+        spec = self.spec
+        if ('+python') in spec:
+            env.prepend_path('PATH', join_path(self.prefix.bin, 'examples', 'python'))
