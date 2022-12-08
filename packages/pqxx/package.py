d1e07c521e27cbcae883d84d171ee24dde29c23c
Bram Veenboer <bram.veenboer@gmail.com>
2022-12-08 10:35:28 +0100

    Add libpqxx

---
+++ packages/pqxx/package.py
@@ -0,0 +1,30 @@
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Pqxx(CMakePackage):
+    """libpqxx is the official C++ client API for PostgreSQL, the
+    enterprise-strength open-source relational database. (If "PostgreSQL" is
+            too verbose, call it by its shorter name, postgres)."""
+
+    homepage = "https://pqxx.org/development/libpqxx/"
+    git      = "https://github.com/jtv/libpqxx.git"
+
+    version('7.7.4', commit='17e5a6c8ac3abc05329891aaa378bd6004b9c8ee')
+    version('6.4.8', commit='d631e84b2f0ecad3f0d9237168ad6100abfbd5b9')
+
+    #
+    # these versions are not recommended:
+    #
+
+    #version('6.3.0', commit='d6ffb1a13e76995253ef698fb8a6b3d4c6499179')
+    # it provides no include/pqxx/config-public-compiler.h
+
+    #version('6.2.5', commit='cb6d840200c0f94bae2616baf6dd4652e272f75e')
+    # fails on "make install"
+
+    depends_on('python')
+    depends_on('postgresql')
