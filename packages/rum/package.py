0bbec76f98e17c7ef166a462b75db2e6d67d9e1b
Stefano Corda <stefano.corda@epfl.ch>
2023-05-15 07:01:32 +0000

    SCOOP-156 add RUM (Resource Usage Monitoring) library

---
+++ packages/rum/package.py
@@ -0,0 +1,34 @@
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Rum(CMakePackage):
+    """ RUM: Resource Usage Monitoring """
+
+    homepage = "https://gitlab.com/ska-telescope/sdp/ska-sdp-rum"
+    git      = "https://gitlab.com/ska-telescope/sdp/ska-sdp-rum"
+
+    version('latest', branch='cpu-stat', preferred=True)
+   
+    variant('python', default=False, description='Enable Python bindings support')
+
+    depends_on('python', when='+python')
+    depends_on('py-pybind11', when='+python')
+
+    def cmake_args(self):
+        spec = self.spec
+        args = [
+            self.define('BUILD_PYTHON_RUM', '+python' in spec),
+        ]
+        return args
+
+    def setup_run_environment(self, env):
+        spec = self.spec
+        if ('+python') in spec:
+            import re
+            python_version = re.search(r'python@([\d].[\d])', str(self.spec)).group(1)
+            env.prepend_path('PYTHONPATH', join_path(self.prefix.lib, "python"))
+
