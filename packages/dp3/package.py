19bc53c4424ddfb33ce7a1c8b87f38f4bc28340d
Bram Veenboer <veenboer@astron.nl>
2024-07-09 14:15:43 +0000

    Add missing dependency on aoflagger for dp3@5.4

--- packages/dp3/package.py
+++ packages/dp3/package.py
@@ -16,9 +16,8 @@ class Dp3(CMakePackage):
     version('5.1', commit='v5.1', submodules=True)
     version('5.2', commit='v5.2', submodules=True)
     version('5.3', commit='v5.3', submodules=True)
-    # 5.4 is not an actual DP3 version, however, WSClean uses this version
-    # in its CI jobs on DAS-6.
-    version('5.4', commit='e52127d5de2ea12fb066eb4544947a4a61a2b0e0', submodules=True)
+    # 5.4 is not an actual DP3 version, however at this commit the DP3_VERSION was set to 5.4.0
+    version('5.4', commit='548828e2fde1a5fc504042b6af384c398b484f79', submodules=True)
     version('6.0', commit='v6.0', submodules=True)
     version('6.1', commit='v6.1', submodules=True)
     version('latest', branch='master', submodules=True)
@@ -27,7 +26,7 @@ class Dp3(CMakePackage):
 
     depends_on('aoflagger@3.4.0', when='@latest')
     depends_on('aoflagger@3.4.0', when='@6.0:')
-    depends_on('aoflagger@3.2.0', when='@5.3')
+    depends_on('aoflagger@3.2.0', when='@5.3:5.4')
     depends_on('aoflagger@3.1.0', when='@5.0:5.2')
     depends_on('everybeam@0.6.0', when='@latest')
     depends_on('everybeam@0.6.0', when='@6.1')
