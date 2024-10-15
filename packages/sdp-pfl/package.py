07489d7168e0bb9e2f8ccf483324de47e4fbc7b3
Saeid Aliei <saeid.aliei@skao.int>
2024-10-15 15:04:55 +0000

    HIP-1075 bump sdp-pfl version and add cfitsio as dependency

--- packages/sdp-pfl/package.py
+++ packages/sdp-pfl/package.py
@@ -18,12 +18,14 @@ class SdpPfl(CMakePackage):
 
     license("BSD-3-Clause")
 
-    version("develop-1.1.7", branch="main", preferred=True)
+    version("develop-1.2.0", branch="main", preferred=True)
+    version("1.2.0", sha256="4991003919aac8045b515cd9cd641d88fc1f886087e5d669f9e2d91b7e6d5b3d")
     version("1.1.7", sha256="b712499e9bf4b79c319b176de4450acfcd28c5edd2406bf8aac640f31db5e796")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("cmake@3.1.0:", type="build")
+    depends_on("cfitsio@4.0.0:", type=("build", "link"))
     
     variant("cuda", default=False, description="Build CUDA kernels")
     variant("cuda_arch", values=("6.0", "6.1", "6.2", "7.0", "7.5", "8.0", "8.6", "8.7"), \
