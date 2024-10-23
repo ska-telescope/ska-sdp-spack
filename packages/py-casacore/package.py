a219c1a34db043bb36a0964130a7dba59e266f87
Saeid Aliei <saeid.aliei@skao.int>
2024-10-23 14:54:37 +0000

    HIP-1075 compile from HEAD of the project

--- packages/py-casacore/package.py
+++ packages/py-casacore/package.py
@@ -11,12 +11,14 @@ class PyCasacore(PythonPackage):
 
     homepage = "https://github.com/casacore/python-casacore"
     url = "https://github.com/casacore/python-casacore/archive/refs/tags/v3.6.1.tar.gz"
+    git = "https://github.com/casacore/python-casacore"
 
     # maintainers("saliei")
 
     license("LPGL-3.0")
 
-    version("3.6.1", sha256="48ca6e8d09d2e822c2bf5286247362d1dfe6d99acbb381676c4b16574959bc03", preferred=True)
+    version("develop-3.6.0", branch="master", preferred=True)
+    version("3.6.1", sha256="48ca6e8d09d2e822c2bf5286247362d1dfe6d99acbb381676c4b16574959bc03")
     version("3.5.2", sha256="ad70c8e08893eec928b3e38c099bda8863f5aa9d099fd00694ad2b0d48eba08f")
     version("3.5.1", sha256="a577233c7311f64a8048180ee82d6946fee16e0dce2976eb516784a32d8b9133")
     version("3.5.0", sha256="47ac85d47051074d64415414212c8c2cfcb49a2037f5c3d78f71ab5b162d1e8b")
@@ -26,6 +28,7 @@ class PyCasacore(PythonPackage):
     depends_on("cxx", type="build")
 
     depends_on("py-scikit-build-core", type="build")
+    depends_on("py-setuptools-scm", type="build")
     depends_on("py-setuptools", type="build")
     depends_on("boost+python", type="build")
     depends_on("cfitsio", type="build")
@@ -33,9 +36,13 @@ class PyCasacore(PythonPackage):
 
     depends_on("casacore+python", type=("build", "link"))
 
-    def setup_build_environment(self, env):
-        # a hack to overcome setuptools bug in not correctly
-        # recognizing correct C++ Intel compiler.
-        if "%oneapi" in self.spec:
-            env.set("CC", os.environ.get("CXX"))
+    @property
+    @llnl.util.lang.memoized
+    def _output_version(self):
+        spec_vers_str = str(self.spec.version.up_to(3))
+        if "develop" in spec_vers_str:
+            # Remove 'develop-' from the version in spack
+            spec_vers_str = spec_vers_str.partition("-")[2]
+        return spec_vers_str
+
 
