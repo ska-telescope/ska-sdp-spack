37a2684f6801c75055489a8249c41d4e4cdb9129
Saeid Aliei <saeid.aliei@skao.int>
2024-11-25 13:57:31 +0000

    HIP-1075 add latest release version and prefer this version

--- packages/py-ska-sdp-func/package.py
+++ packages/py-ska-sdp-func/package.py
@@ -18,7 +18,9 @@ class PySkaSdpFunc(PythonPackage):
 
     license("BSD-3-Clause")
 
-    version("develop-1.2.0", branch="main", preferred=True)
+    version("develop-1.2.2", branch="main")
+    version("1.2.2", sha256="7d40b3f8d0f18199a3ea85d4123af911a021a4e62a51140eac754c80f72a6c2c", preferred=True)
+    version("1.2.1", sha256="ec1376d171f3130feb679fcad18d7783ce553fa9d75381ce7d4811a4005e98f3")
     version("1.2.0", sha256="4991003919aac8045b515cd9cd641d88fc1f886087e5d669f9e2d91b7e6d5b3d")
     version("1.1.7", sha256="b712499e9bf4b79c319b176de4450acfcd28c5edd2406bf8aac640f31db5e796")
 
