4faa6bfbf3687d58866d9cfd94be1ffb39241e52
Saeid Aliei <Saeid.Aliei@skao.int>
2024-12-17 11:52:15 +0000

    HIP-1120 add icu4c as build dependency

--- packages/pqxx-autotools/package.py
+++ packages/pqxx-autotools/package.py
@@ -7,12 +7,13 @@ from spack import *
 
 
 class PqxxAutotools(AutotoolsPackage):
-    """libpqxx is the official C++ client API for PostgreSQL, the
-    enterprise-strength open-source relational database. (If "PostgreSQL" is
-            too verbose, call it by its shorter name, postgres)."""
+    """libpqxx is the official C++ client API for PostgreSQL, 
+    the enterprise-strength open-source relational database. 
+    (If "PostgreSQL" is too verbose, call it by its shorter name, postgres)."""
 
     homepage = "https://pqxx.org/development/libpqxx/"
     git      = "https://github.com/jtv/libpqxx.git"
+    parallel = False
 
     version('5.1.1', commit='ae16ffc87c7eeec8fa936ca1c75c5660761be619')
     version('5.0.1', commit='0e93d8ebf542a298dfbd4afdc419113b246a093a')
@@ -23,6 +24,7 @@ class PqxxAutotools(AutotoolsPackage):
     depends_on('python')
     depends_on('doxygen')
     depends_on('xmlto')
+    depends_on("icu4c", type=("build"))
 
     def install(self, spec, prefix):
         configure("--prefix=" + prefix)
@@ -39,5 +41,3 @@ class PqxxAutotools(AutotoolsPackage):
             args.append("--disable-documentation")
         args.append("--enable-shared")
         return args
-
-    parallel = False
