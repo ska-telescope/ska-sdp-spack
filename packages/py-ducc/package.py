2a40c387fbbd744d4a7881715461fcb6fc499c6a
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2024-12-20 12:49:37 +0100

    AST-1640 Remove ducc copyright and license headers

--- packages/py-ducc/package.py
+++ packages/py-ducc/package.py
@@ -1,8 +1,3 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 from spack.package import *
 import os
 
@@ -49,4 +44,3 @@ class PyDucc(PythonPackage):
         # recognizing correct C++ Intel compiler.
         if "%oneapi" in self.spec:
             env.set("CC", os.environ.get("CXX"))
-
