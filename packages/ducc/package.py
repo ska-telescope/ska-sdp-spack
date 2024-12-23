9d956d940d99f538eae5d3c40115f04c9e94a540
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2024-12-23 09:09:37 +0000

    AST-1640 Update import statements; Remove copyright and license headers

--- packages/ducc/package.py
+++ packages/ducc/package.py
@@ -1,16 +1,11 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 from spack.package import *
 
 
 class Ducc(CMakePackage):
-    """Distinctly Useful Code Collection (DUCC), is a collection of basic programming 
-    tools for numerical computation, including Fast Fourier Transforms, Spherical Harmonic Transforms, 
+    """Distinctly Useful Code Collection (DUCC), is a collection of basic programming
+    tools for numerical computation, including Fast Fourier Transforms, Spherical Harmonic Transforms,
     non-equispaced Fourier transforms, as well as some concrete applications like 4pi convolution
-    on the sphere and gridding/degridding of radio interferometry data. Note that this 
+    on the sphere and gridding/degridding of radio interferometry data. Note that this
     package doesn't install the python interface."""
 
     homepage = "https://gitlab.mpcdf.mpg.de/mtr/ducc"
@@ -41,4 +36,3 @@ class Ducc(CMakePackage):
             # Remove 'develop-' from the version in spack
             spec_vers_str = spec_vers_str.partition("-")[2]
         return spec_vers_str
-
