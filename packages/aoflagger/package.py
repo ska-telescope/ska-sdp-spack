d4fcf4a644b55c3649b14b07c2e11697ae31a7b8
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2025-01-08 11:42:22 +0000

    AST-1640 Make aoflagger depend on Python

--- packages/aoflagger/package.py
+++ packages/aoflagger/package.py
@@ -1,4 +1,4 @@
-from spack.package import *
+from spack.package import CMakePackage, depends_on, variant, version
 
 
 class Aoflagger(CMakePackage):
@@ -6,33 +6,37 @@ class Aoflagger(CMakePackage):
     for astronomical radio observations."""
 
     homepage = "https://sourceforge.net/projects/aoflagger/"
-    git      = "https://gitlab.com/aroffringa/aoflagger.git"
-    version('3.1.0', commit='v3.1.0', submodules=True)
-    version('3.2.0', commit='v3.2.0', submodules=True)
-    version('3.3.0', commit='v3.3.0', submodules=True)
-    version('3.4.0', commit='v3.4.0', submodules=True)
-    version('latest', branch='master', submodules=True)
+    git = "https://gitlab.com/aroffringa/aoflagger.git"
+    version("3.1.0", commit="v3.1.0", submodules=True)
+    version("3.2.0", commit="v3.2.0", submodules=True)
+    version("3.3.0", commit="v3.3.0", submodules=True)
+    version("3.4.0", commit="v3.4.0", submodules=True)
+    version("latest", branch="master", submodules=True)
 
+    variant(
+        "debug-information",
+        default=False,
+        description="Enable debug information",
+    )
+    variant("gui", default=False, description="Build rfigui and aoqplot tools")
 
-    variant('debug-information', default=False, description='Enable debug information')
-    variant('gui', default=False, description='Build rfigui and aoqplot tools')
-
-    depends_on('casacore+data')
-    depends_on('cfitsio')
-    depends_on('fftw')
-    depends_on('boost+python+numpy+date_time')
-    depends_on('hdf5')
-    depends_on('libxml2')
-    depends_on('lapack')
-    depends_on('lua')
-    depends_on('cairo')
-    depends_on('git')
+    depends_on("casacore+data")
+    depends_on("cfitsio")
+    depends_on("fftw")
+    depends_on("boost+python+numpy+date_time")
+    depends_on("hdf5")
+    depends_on("libxml2")
+    depends_on("lapack")
+    depends_on("lua")
+    depends_on("cairo")
+    depends_on("git")
+    depends_on("python")
 
     def cmake_args(self):
         spec = self.spec
         args = [
-            self.define('ENABLE_GUI', '+gui' in spec),
+            self.define("ENABLE_GUI", "+gui" in spec),
         ]
-        if '+debug-information' in spec:
-            args.append(self.define('CMAKE_CXX_FLAGS', "-g"))
+        if "+debug-information" in spec:
+            args.append(self.define("CMAKE_CXX_FLAGS", "-g"))
         return args
