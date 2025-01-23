b64e76e647ba1cec665bb7bdece0dea43e69ea30
Chiheb <chiheb.sakka@oca.eu>
2025-01-23 16:24:13 +0000

    SCOOP-355:add benchmon monitoring tool and  dependencies

---
+++ packages/dool/package.py
@@ -0,0 +1,17 @@
+from spack import *
+
+class Dool(Package):
+    """Dool is a command-line tool for monitoring various aspects of your Linux 
+    system, such as CPU, Memory, Network, Load Average, etc. It features a robust 
+    plug-in architecture for monitoring additional metrics. This is a Python3 
+    compatible fork of Dstat."""
+
+    homepage = "https://github.com/scottchiefbaker/dool"
+    git      = "https://github.com/scottchiefbaker/dool.git"
+
+    # Specify versions
+    version('latest', branch='master', preferred=True)
+    
+    def install(self, spec, prefix):
+        """Since the repository contains ready-to-use scripts, we simply clone it into the prefix."""
+        install_tree('.', prefix)
