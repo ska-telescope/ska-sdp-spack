f14691fa71d9092572b0936f4665f5d08d8d2e0a
Chiheb <chiheb.sakka@oca.eu>
2025-01-29 15:35:56 +0000

    SCOOP-355: Formatting packages for pylint compatibility

--- packages/py-ping3/package.py
+++ packages/py-ping3/package.py
@@ -4,7 +4,7 @@ class PyPing3(PythonPackage):
     """Ping3 is a pure Python 3 implementation of ICMP ping."""
 
     homepage = "https://github.com/kyan001/ping3"
-    url      = "https://files.pythonhosted.org/packages/source/p/ping3/ping3-4.0.3.tar.gz"  
+    url= "https://files.pythonhosted.org/packages/source/p/ping3/ping3-4.0.3.tar.gz"
     
     version('4.0.3', sha256='372a5c26c6aeebf5da310df7660ee6204146edc46f1bad19e349ae57cadb1d94')
     version('develop', branch='main',git='https://github.com/jarhainx/python-ping3.git')
