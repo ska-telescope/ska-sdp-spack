b727249d08ee345b195800bbe160223848ffd8d2
Chiheb <chiheb.sakka@oca.eu>
2025-01-30 16:14:40 +0000

    SCOOP-355: remove the explicit import of depends_on  and version

--- packages/py-ska-sdp-benchmark-monitor/package.py
+++ packages/py-ska-sdp-benchmark-monitor/package.py
@@ -1,4 +1,4 @@
-from spack.package import PythonPackage, depends_on, version
+from spack.package import PythonPackage
 
 class PySkaSdpBenchmarkMonitor(PythonPackage):
     """BenchMon is a monitoring tool designed to collect
