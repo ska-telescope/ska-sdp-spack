1c944d6c767f421c4239000b14349d493236d6e1
Chiheb <chiheb.sakka@oca.eu>
2025-01-31 12:36:51 +0000

    SCOOP-355: debug pylint

--- packages/py-ping3/package.py
+++ packages/py-ping3/package.py
@@ -1,15 +1,16 @@
-import llnl.util.lang
-from spack.package import PythonPackage, depends_on, version
+import spack.build_systems.autotools
+from spack.package import *
+#from spack.package import PythonPackage, depends_on, version
 
 
 class PyPing3(PythonPackage):
     """Ping3 is a pure Python 3 implementation of ICMP ping."""
 
-    # homepage = "https://github.com/kyan001/ping3"
-    # url= "https://files.pythonhosted.org/packages/source/p/ping3/ping3-4.0.3.tar.gz"
-    # version('4.0.3', sha256='372a5c26c6aeebf5da310df7660ee6204146edc46f1bad19e349ae57cadb1d94')
-    # version('develop', branch='main',git='https://github.com/jarhainx/python-ping3.git')
+    homepage = "https://github.com/kyan001/ping3"
+    url= "https://files.pythonhosted.org/packages/source/p/ping3/ping3-4.0.3.tar.gz"
+    version('4.0.3', sha256='372a5c26c6aeebf5da310df7660ee6204146edc46f1bad19e349ae57cadb1d94')
+    version('develop', branch='main',git='https://github.com/jarhainx/python-ping3.git')
 
-    # depends_on('python@3.6:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
+    depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
     
--- packages/py-ska-sdp-benchmark-monitor/package.py
+++ packages/py-ska-sdp-benchmark-monitor/package.py
@@ -1,18 +1,18 @@
 from spack.package import PythonPackage
 
-# class PySkaSdpBenchmarkMonitor(PythonPackage):
-#     """BenchMon is a monitoring tool designed to collect
-#     hardware and software context as well as measure benchmark runs.
-#     """
+class PySkaSdpBenchmarkMonitor(PythonPackage):
+    """BenchMon is a monitoring tool designed to collect
+    hardware and software context as well as measure benchmark runs.
+    """
     
-#     homepage = "https://gitlab.com/ska-telescope/sdp/ska-sdp-benchmark-monitor"
-#     git      = "https://gitlab.com/ska-telescope/sdp/ska-sdp-benchmark-monitor.git"
+    homepage = "https://gitlab.com/ska-telescope/sdp/ska-sdp-benchmark-monitor"
+    git      = "https://gitlab.com/ska-telescope/sdp/ska-sdp-benchmark-monitor.git"
 
-#     # versions
-#     version('latest',branch='main',preferred=True) 
+    # versions
+    version('latest',branch='main',preferred=True) 
 
-#     depends_on('python', type=('build', 'run'))
-#     depends_on('py-psutil', type='run')
-#     depends_on('py-ping3', type='run')
-#     depends_on('py-numpy', type='run')
-#     depends_on('py-matplotlib', type='run')
+    depends_on('python', type=('build', 'run'))
+    depends_on('py-psutil', type='run')
+    depends_on('py-ping3', type='run')
+    depends_on('py-numpy', type='run')
+    depends_on('py-matplotlib', type='run')
