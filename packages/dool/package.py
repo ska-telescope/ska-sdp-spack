2416c237ca365c0ff0a9ae5e93e94e0c3ea1bc6e
Chiheb <chiheb.sakka@oca.eu>
2025-02-03 09:17:21 +0000

    SCOOP-355: debug: dool: reformat package.py

--- packages/dool/package.py
+++ packages/dool/package.py
@@ -1,17 +1,16 @@
 from spack.package import PythonPackage, depends_on, version
 
-class Dool(Package):
-    """Dool is a command-line tool for monitoring various aspects of your Linux 
-    system, such as CPU, Memory, Network, Load Average, etc. It features a robust 
-    plug-in architecture for monitoring additional metrics. This is a Python3 
+
+class Dool(PythonPackage):
+    """Dool is a command-line tool for monitoring various aspects of your Linux
+    system, such as CPU, Memory, Network, Load Average, etc. It features a robust
+    plug-in architecture for monitoring additional metrics. This is a Python3
     compatible fork of Dstat."""
 
     homepage = "https://github.com/scottchiefbaker/dool"
-    git      = "https://github.com/scottchiefbaker/dool.git"
+    git = "https://github.com/scottchiefbaker/dool.git"
 
     # Specify versions
-    version('latest', branch='master', preferred=True)
-    
-    def install(self, spec, prefix):
-        """Since the repository contains ready-to-use scripts, we simply clone it into the prefix."""
-        install_tree('.', prefix)
+    version("latest", branch="master", preferred=True)
+
+    depends_on("py-setuptools", type="build")
