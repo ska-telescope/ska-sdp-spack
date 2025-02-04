3d1309a42a6b12c9f762c5a38b7f263e53f9f3a7
Vincent Morello <vmorello@gmail.com>
2025-02-04 13:17:09 +0000

    HIP-1164: Update BPP package after adding dask distribution

--- packages/py-ska-sdp-batch-preprocess/package.py
+++ packages/py-ska-sdp-batch-preprocess/package.py
@@ -32,9 +32,13 @@ class PySkaSdpBatchPreprocess(PythonPackage):
     license("BSD 3-Clause")
 
     version("latest", branch="main", preferred=True)
+    version("2.2.0", commit="98747f8fa7064c770cd2b7b9e6232f0fdc722ea4")
+    version("2.1.1", commit="0754ad2ca1fa67978ba30af8a38f03189e158c1f")
 
     depends_on("python@3.10:", type=("build", "run"))
     depends_on("py-pyyaml@6.0.1:", type=("build", "run"))
     depends_on("py-jsonschema@4.4.0:", type=("build", "run"))
     depends_on("py-h5py@3.7.0:", type=("build", "run"))
+    depends_on("py-dask@2024.7.1:", type=("build", "run"))
+    depends_on("py-distributed@2024.7.1:", type=("build", "run"))
     depends_on("dp3@6.0:", type=("build", "run"))
