53c43464ac249ee564a04ecefb5b40f8d31775fe
Nimalan M <m.nimalan@thoughtworks.com>
2025-02-04 19:04:13 +0530

    DHR-184: Add missing runtime deps

--- packages/py-ska-sdp-e2e-batch-continuum-imaging/package.py
+++ packages/py-ska-sdp-e2e-batch-continuum-imaging/package.py
@@ -18,10 +18,13 @@ class PySkaSdpE2eBatchContinuumImaging(PythonPackage):
 
     maintainers("nimalan", "justin")
 
-    license("BSD-3-Clause", checked_by="scpmw")
+    license("BSD 3-Clause")
 
     version("0.1.0", commit="618168aee1aea8e991063fd2cdd079cda03505ef")
     version("latest", branch="main")
 
     depends_on("python@3.10", type=("build", "run"))
     depends_on("py-poetry-core", type="build")
+    depends_on("py-yaml@6.0.2:", type="run")
+    depends_on("py-dask@2023.7.1:+distributed", type="run")
+    depends_on("py-typer@0.12.5:", type="run")
