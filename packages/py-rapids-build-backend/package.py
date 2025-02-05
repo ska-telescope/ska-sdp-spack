b66430ccb084abca124fed8738052a095774b2a2
Saeid Aliei <saeid.aliei@skao.int>
2025-02-05 15:55:17 +0000

    HIP-1122 change from latest to main version specifier

--- packages/py-rapids-build-backend/package.py
+++ packages/py-rapids-build-backend/package.py
@@ -16,7 +16,7 @@ class PyRapidsBuildBackend(PythonPackage):
 
     license("Apache-2.0-License")
 
-    version("latest", branch="main")
+    version("main", branch="main")
     version("0.3.2", sha256="11ed069adf2b6b70b8f45e5d8e3947ed8f669e4cd7e7245f491b86f0c1f0a01d", preferred=True)
     version("0.3.1", sha256="0d97ab05dc7bb6180b8788d29ca4d4855119792f31217c8d71af23b83792dfeb")
     version("0.3.0", sha256="98f58ece94040b8678cb2a00ef5a8cfdced00fd3fdc0fa41ed45ffe04d84981d")
