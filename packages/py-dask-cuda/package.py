d0439392007bf2869a8607cdfd53e206c9d63089
Saeid Aliei <Saeid.Aliei@skao.int>
2025-02-07 10:37:09 +0000

    HIP-1170 fix formatting

--- packages/py-dask-cuda/package.py
+++ packages/py-dask-cuda/package.py
@@ -16,33 +16,40 @@ class PyDaskCuda(PythonPackage):
 
     homepage = "https://docs.rapids.ai/api/dask-cuda/stable/"
     git = "https://github.com/rapidsai/dask-cuda"
-    url = \
-    "https://github.com/rapidsai/dask-cuda/archive/refs/tags/v24.12.00.tar.gz"
+    url = (
+        "https://github.com/rapidsai/dask-cuda/archive/refs/tags/v24.12.00.tar.gz"
+    )
 
     maintainers("saliei")
 
     license("Apache-2.0-License")
 
     version("develop-25.04", branch="branch-25.04")
-    version("24.12.00",
-            sha256=\
-            "0e72ee7b0b894fea70b6f39944ffb0f0cdcb6db78d6a891774e14f4965db8d10", 
-            preferred=True)
-    version("24.10.00",
-            sha256=\
-            "7e3f8ece55023005b1d09f6a73cdea4aa802f5b62adfd5480423808087a20841")
-    version("24.08.00",
-            sha256=\
-            "ab65debe72e8ed8d3df573a3e849672104556e9b144ff910820a0cea09cb3fc8")
-    version("24.06.00",
-            sha256=\
-            "8f46f34deb2c0d692d9d1aefbdbd7a7c5daaee35b330322ac3c202c8102fc66b")
-    version("24.04.00",
-            sha256=\
-            "5a3493c60cef5796d615473acaca416d9a6c685b85c1b601262b34c9431b36eb")
-    version("24.02.00",
-            sha256=\
-            "6cdfe600c6cdd2a3fb82d6a58dd2acfed2c82f4b32b2ab5611d2cc9292fd2f2a")
+    version(
+        "24.12.00",
+        sha256="0e72ee7b0b894fea70b6f39944ffb0f0cdcb6db78d6a891774e14f4965db8d10",
+        preferred=True,
+    )
+    version(
+        "24.10.00",
+        sha256="7e3f8ece55023005b1d09f6a73cdea4aa802f5b62adfd5480423808087a20841",
+    )
+    version(
+        "24.08.00",
+        sha256="ab65debe72e8ed8d3df573a3e849672104556e9b144ff910820a0cea09cb3fc8",
+    )
+    version(
+        "24.06.00",
+        sha256="8f46f34deb2c0d692d9d1aefbdbd7a7c5daaee35b330322ac3c202c8102fc66b",
+    )
+    version(
+        "24.04.00",
+        sha256="5a3493c60cef5796d615473acaca416d9a6c685b85c1b601262b34c9431b36eb",
+    )
+    version(
+        "24.02.00",
+        sha256="6cdfe600c6cdd2a3fb82d6a58dd2acfed2c82f4b32b2ab5611d2cc9292fd2f2a",
+    )
 
     depends_on("python", type=("build", "run"))
     depends_on("py-setuptools", type="build")
